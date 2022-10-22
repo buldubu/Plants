47ae2bfccb9b365823cc430cd0a451b7e3ffcb41
buldubu <buldubu@gmail.com>
2022-10-22 21:51:48 +0300

    Scene gecisleri

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
-    public GameObject entry;
-    public GameObject entry_2;
-    Vector3 entry_point;
-    Vector3 entry_point_2;
+    public GameObject spawn;
+    public GameObject spawn_2;
+    Vector3 spawn_point;
+    Vector3 spawn_point_2;
     public static Player player;
 
     int scene_prev;
@@ -32,37 +32,37 @@ public class Player : MonoBehaviour
     {
         scene_prev = SceneManager.GetActiveScene().buildIndex;
         scene_current = scene_prev;
-        entry = GameObject.Find("entry");
-        entry_point = entry.transform.position;
-        this.transform.position = new Vector3(entry_point.x, entry_point.y, 0);
+        spawn = GameObject.Find("spawn");
+        spawn_point = spawn.transform.position;
+        this.transform.position = new Vector3(spawn_point.x, spawn_point.y, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         scene_current = SceneManager.GetActiveScene().buildIndex;
-        if (scene_prev > scene_current)
+        if (scene_prev < scene_current)
         {
             change_1 = true;
             scene_prev = scene_current;
         }
-        else if (scene_prev < scene_current)
+        else if (scene_prev > scene_current)
         {
             change_2 = true;
             scene_prev = scene_current;
         }
         if (change_1)
         {
-            entry = GameObject.Find("entry");
-            entry_point = entry.transform.position;
-            this.transform.position = new Vector3(entry_point.x, entry_point.y, 0);
+            spawn = GameObject.Find("spawn");
+            spawn_point = spawn.transform.position;
+            this.transform.position = new Vector3(spawn_point.x, spawn_point.y, 0);
             change_1 = false;
         }
         else if (change_2)
         {
-            entry = GameObject.Find("entry_2");
-            entry_point = entry.transform.position;
-            this.transform.position = new Vector3(entry_point.x, entry_point.y, 0);
+            spawn_2 = GameObject.Find("spawn_2");
+            spawn_point_2 = spawn_2.transform.position;
+            this.transform.position = new Vector3(spawn_point_2.x, spawn_point_2.y, 0);
             change_2 = false;
         }
     }
